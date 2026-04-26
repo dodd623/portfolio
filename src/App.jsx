5a76646e665bb39b60063647fcf4eb1fdbeab518
Devin J Dodd <dodd623@gmail.com>
2026-04-26 19:33:34 -0400

    Updated Portfolio image

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ export default function App() {
   );
 
   const resumeLink = "/Devin_Dodd_Resume.pdf";
-  const headshot = "/Portfolio-Headshot.jpg";
+  const headshot = "/LinkedIn PFP.jpg";
 
   const skills = [
     {
